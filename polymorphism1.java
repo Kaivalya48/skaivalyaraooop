d0f9ef30612126786d50bad625c728a664887103
Kaivalya48 <59616211+Kaivalya48@users.noreply.github.com>
2020-02-08 22:34:40 +0530

    Update polymorphism1.java

--- polymorphism1.java
+++ polymorphism1.java
@@ -3,9 +3,9 @@ class Sbi{
     void display(String user,int balance){
         System.out.println("****************************");
         System.out.println("HI "+ user);
-        System.out.println("WELCOME To SBI BANK");
+        System.out.println("WELCOME TO SBI BANK");
         System.out.println("YOUR ACCOUNT BALANCE IS " + balance);
-        System.out.println("THANKS FOR USING SBI BANK");
+        System.out.println("THANK YOU FOR USING SBI BANK");
         System.out.println("****************************");
         
     }
@@ -24,8 +24,8 @@ class Sbi{
 public class Test{
     public static void main(String args[]){
         Sbi a=new Sbi();
-        a.display("SAi",5000);
-        a.display("SAi",5000,2000);
+        a.display("Satya",5000);
+        a.display("Satya",5000,2000);
         
     }
 }
